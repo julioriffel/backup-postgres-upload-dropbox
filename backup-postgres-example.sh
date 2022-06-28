34c5d17dd90bc80ef6fd4bea8dd5732e10558d4c
Julio Cezar Riffel <julio.riffel@orama.com.br>
2022-06-28 01:06:52 -0300

    Update to Oauth2 III

--- backup-postgres-example.sh
+++ backup-postgres-example.sh
@@ -1,9 +1,9 @@
 #Create Folder
+cd /home/user/labs/backup-postgres/
 mkdir -p "files"
 
 #backup Postgres
 pg_dump --no-owner -h painel.bolsadedados.com.br -p 5432 -U postgres database | gzip > /home/user/labs/backup-postgres/files/alerta_$(date +%d-%m-%y-%H-%M-%S).gz
 
 #Upload to Dropbox
-cd /home/user/labs/backup-postgres/
 /home/user/.local/share/virtualenvs/backup-postgres-wR48kqji/bin/python /home/user/labs/backup-postgres/dropbox_upload.py
